6c7f2f4f34517077df93b2a1ea5d50805cdcc73f
MarcelaIsmodes <a20253389@pucp.edu.pe>
2025-06-27 19:14:10 -0500

    PC4.py

--- PC4.py
+++ PC4.py
@@ -135,7 +135,7 @@ elif  pagina_seleccionada == 'Experiencia':
     # En este caso, {texto_2} se reemplaza por el valor de la variable texto.
 
     # Agregamos un subtítulo para el video
-    st.markdown("<h2 style='text-align: center;'>PC1 y PC2</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center;'>Declaraciones condicionales y bucles</h2>", unsafe_allow_html=True)
 
     
     # <h2 style='text-align: center;'>Aquí escribe un nombre creativo para presentar tu video</h2>: Esta es una cadena de código HTML.
